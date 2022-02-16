aa761aaf3d9876b430969789bd0a4c357d7cd3df
Nacho Gomez <jimbo.greender@gmail.com>
2022-02-16 20:09:20 +0100

    commit con dababase=update con data.sql comentado

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,127 +1,127 @@
-INSERT INTO composer(artistic_name) VALUES('Jimbo Jonez');
-INSERT INTO composer(artistic_name) VALUES('Aitortxu');
-INSERT INTO album(name, composer_id) VALUES('El Caso Jonez', 1);
-
-INSERT INTO song(release_date, tittle, url, videoclip, album_id)
-VALUES('2017-11-17', 'Introspective Revolution', 'https://open.spotify.com/track/4S9aDnIoqLgMfJTSJugyEw?si=5a4f77b5e63b4c82', null, 1);
-INSERT INTO song_composer(song_id, composer_id)
-VALUES(1,1);
-
-INSERT INTO song(release_date, tittle, url, videoclip, album_id)
-VALUES('2017-11-17', 'Crear Por Crear', 'https://open.spotify.com/track/5Y7dSQrBHCEo3FaAL7UoEo?si=250cdf66130d4666', 'https://www.youtube.com/watch?v=hA9cRoTlUGE', 1);
-INSERT INTO song_composer(song_id, composer_id)
-VALUES(2,1);
-INSERT INTO song_composer(song_id, composer_id)
-VALUES(2,2);
-
-INSERT INTO song(release_date, tittle, url, videoclip, album_id)
-VALUES('2017-11-17', 'No Te Conocías', 'https://open.spotify.com/track/2Caq4k0aYrQwPrJaUO4RIR?si=217f728c224d4114', null, 1);
-INSERT INTO song_composer(song_id, composer_id)
-VALUES(3,1);
-
-INSERT INTO song(release_date, tittle, url, videoclip, album_id)
-VALUES('2017-11-17', 'Eres', 'https://open.spotify.com/track/1xY7RIWTGSFz0IxfHfuaLg?si=2c554e4d9a69402c', 'https://www.youtube.com/watch?v=EQalQ2AvE2o', 1);
-INSERT INTO song_composer(song_id, composer_id)
-VALUES(4,1);
-
-INSERT INTO composer(artistic_name) VALUES('Mehding');
-
-INSERT INTO song(release_date, tittle, url, videoclip, album_id)
-VALUES('2017-11-17', 'Blood Borders', 'https://open.spotify.com/track/69nqQUvsW8Ldkno8TaFNRy?si=a1c7ff4383464227', null, 1);
-INSERT INTO song_composer(song_id, composer_id)
-VALUES(5,1), (5,3);
-
-INSERT INTO song(release_date, tittle, url, videoclip, album_id)
-VALUES('2017-11-17', 'Insternship Inna Dub', 'https://open.spotify.com/track/1tTppgxHAFh9qcagAJ1axk?si=a52a5eed53404b76', null, 1);
-INSERT INTO song_composer(song_id, composer_id)
-VALUES(6,1);
-
-INSERT INTO composer(artistic_name) VALUES('Sergio Jackson');
-
-INSERT INTO song(release_date, tittle, url, videoclip, album_id)
-VALUES('2017-11-17', 'Generación Perdida', 'https://open.spotify.com/track/5DsKaKaLRFOz75sQ2GMBKG?si=744e6e33d1234773', 'https://www.youtube.com/watch?v=kc3sdZlkf-o', 1);
-INSERT INTO song_composer(song_id, composer_id)
-VALUES(7,1), (7,2), (7,4);
-
-INSERT INTO song(release_date, tittle, url, videoclip, album_id)
-VALUES('2017-11-17', 'Small Seeds', 'https://open.spotify.com/track/5RI2qZ4blYntOl87Qz7lVy?si=82e5e4c1fa9a4b46', null, 1);
-INSERT INTO song_composer(song_id, composer_id)
-VALUES(8,1);
-
-INSERT INTO song(release_date, tittle, url, videoclip, album_id)
-VALUES('2017-11-17', 'Sentir', 'https://open.spotify.com/track/66xpZoRJntYBOFHNLuR5CY?si=8d29709599154af1', null, 1);
-INSERT INTO song_composer(song_id, composer_id)
-VALUES(9,1);
-
-INSERT INTO composer(artistic_name) VALUES('Skiz Sie7e');
-
-INSERT INTO song(release_date, tittle, url, videoclip, album_id)
-VALUES('2017-11-17', 'No Me Da Vergüenza', 'https://open.spotify.com/track/13FX5JJS6YMLcog5VSY5tc?si=6158c30ca5954b95', null, 1);
-INSERT INTO song_composer(song_id, composer_id)
-VALUES(10,1), (10,5);
-
-INSERT INTO composer(artistic_name) VALUES('Sisi Greenders');
-
-INSERT INTO song(release_date, tittle, url, videoclip, album_id)
-VALUES('2017-11-17', 'Naieh', 'https://open.spotify.com/track/5BKfjlUT2rXxT5rpynTuuk?si=2854d16f9e174d46', null, 1);
-INSERT INTO song_composer(song_id, composer_id)
-VALUES(11,1), (11,6);
-
-INSERT INTO song(release_date, tittle, url, videoclip, album_id)
-VALUES('2017-11-17', 'Funk Creator', 'https://open.spotify.com/track/4kyzw6GFML34xus4by9jb7?si=14a4040517c44f3e', null, 1);
-INSERT INTO song_composer(song_id, composer_id)
-VALUES(12,1);
-
-INSERT INTO song(release_date, tittle, url, videoclip, album_id)
-VALUES('2017-11-17', 'Dont Know Blues', 'https://open.spotify.com/track/0KqrqwHdPu08ENhyOXXlj3?si=3b636dbf9a224ad6', null, 1);
-INSERT INTO song_composer(song_id, composer_id)
-VALUES(13,1);
-
-INSERT INTO song(release_date, tittle, url, videoclip, album_id)
-VALUES('2017-11-17', 'Seeds Riddim', 'https://open.spotify.com/track/13FX5JJS6YMLcog5VSY5tc?si=6158c30ca5954b95', null, 1);
-INSERT INTO song_composer(song_id, composer_id)
-VALUES(14,1);
-
-INSERT INTO song(release_date, tittle, url, videoclip, album_id)
-VALUES('2017-11-17', 'Out Of Shame', 'https://open.spotify.com/track/13FX5JJS6YMLcog5VSY5tc?si=6158c30ca5954b95', null, 1);
-INSERT INTO song_composer(song_id, composer_id)
-VALUES(15,1);
-
-INSERT INTO album(name, composer_id) VALUES('Individuo', 2);
-
-INSERT INTO song(release_date, tittle, url, videoclip, album_id)
-VALUES('2020-05-28', 'Proemio', 'https://aitortxumusica.bandcamp.com/track/proemio', null, 2);
-INSERT INTO song_composer(song_id, composer_id)
-VALUES(16,2);
-
-INSERT INTO song(release_date, tittle, url, videoclip, album_id)
-VALUES('2020-05-28', 'El Individuo', 'https://aitortxumusica.bandcamp.com/track/el-individuo', 'https://www.youtube.com/watch?v=vpIPpCGc9_s', 2);
-INSERT INTO song_composer(song_id, composer_id)
-VALUES(17,2);
-
-INSERT INTO song(release_date, tittle, url, videoclip, album_id)
-VALUES('2020-05-28', 'El Eco', 'https://aitortxumusica.bandcamp.com/track/el-eco', null, 2);
-INSERT INTO song_composer(song_id, composer_id)
-VALUES(18,2);
-
-INSERT INTO song(release_date, tittle, url, videoclip, album_id)
-VALUES('2020-05-28', 'La Huida', 'https://aitortxumusica.bandcamp.com/track/la-huida-con-jimbo-jonez', 'https://www.youtube.com/watch?v=4P2sCBtR3Qk', 2);
-INSERT INTO song_composer(song_id, composer_id)
-VALUES(19,2),(19,1);
-
-INSERT INTO composer(artistic_name) VALUES('Soul Rebel');
-
-INSERT INTO song(release_date, tittle, url, videoclip, album_id)
-VALUES('2020-05-28', 'Destierro', 'https://aitortxumusica.bandcamp.com/track/destierro-con-soul-rebel', null, 2);
-INSERT INTO song_composer(song_id, composer_id)
-VALUES(20,2), (20,7);
-
-INSERT INTO song(release_date, tittle, url, videoclip, album_id)
-VALUES('2020-05-28', 'Renuncia', 'https://aitortxumusica.bandcamp.com/track/renuncia', null, 2);
-INSERT INTO song_composer(song_id, composer_id)
-VALUES(21,2);
-
-INSERT INTO song(release_date, tittle, url, videoclip, album_id)
-VALUES('2020-05-28', 'Redención', 'https://aitortxumusica.bandcamp.com/track/redenci-n', null, 2);
-INSERT INTO song_composer(song_id, composer_id)
-VALUES(22,2);
+--INSERT INTO composer(artistic_name) VALUES('Jimbo Jonez');
+--INSERT INTO composer(artistic_name) VALUES('Aitortxu');
+--INSERT INTO album(name, composer_id) VALUES('El Caso Jonez', 1);
+--
+--INSERT INTO song(release_date, tittle, url, videoclip, album_id)
+--VALUES('2017-11-17', 'Introspective Revolution', 'https://open.spotify.com/track/4S9aDnIoqLgMfJTSJugyEw?si=5a4f77b5e63b4c82', null, 1);
+--INSERT INTO song_composer(song_id, composer_id)
+--VALUES(1,1);
+--
+--INSERT INTO song(release_date, tittle, url, videoclip, album_id)
+--VALUES('2017-11-17', 'Crear Por Crear', 'https://open.spotify.com/track/5Y7dSQrBHCEo3FaAL7UoEo?si=250cdf66130d4666', 'https://www.youtube.com/watch?v=hA9cRoTlUGE', 1);
+--INSERT INTO song_composer(song_id, composer_id)
+--VALUES(2,1);
+--INSERT INTO song_composer(song_id, composer_id)
+--VALUES(2,2);
+--
+--INSERT INTO song(release_date, tittle, url, videoclip, album_id)
+--VALUES('2017-11-17', 'No Te Conocías', 'https://open.spotify.com/track/2Caq4k0aYrQwPrJaUO4RIR?si=217f728c224d4114', null, 1);
+--INSERT INTO song_composer(song_id, composer_id)
+--VALUES(3,1);
+--
+--INSERT INTO song(release_date, tittle, url, videoclip, album_id)
+--VALUES('2017-11-17', 'Eres', 'https://open.spotify.com/track/1xY7RIWTGSFz0IxfHfuaLg?si=2c554e4d9a69402c', 'https://www.youtube.com/watch?v=EQalQ2AvE2o', 1);
+--INSERT INTO song_composer(song_id, composer_id)
+--VALUES(4,1);
+--
+--INSERT INTO composer(artistic_name) VALUES('Mehding');
+--
+--INSERT INTO song(release_date, tittle, url, videoclip, album_id)
+--VALUES('2017-11-17', 'Blood Borders', 'https://open.spotify.com/track/69nqQUvsW8Ldkno8TaFNRy?si=a1c7ff4383464227', null, 1);
+--INSERT INTO song_composer(song_id, composer_id)
+--VALUES(5,1), (5,3);
+--
+--INSERT INTO song(release_date, tittle, url, videoclip, album_id)
+--VALUES('2017-11-17', 'Insternship Inna Dub', 'https://open.spotify.com/track/1tTppgxHAFh9qcagAJ1axk?si=a52a5eed53404b76', null, 1);
+--INSERT INTO song_composer(song_id, composer_id)
+--VALUES(6,1);
+--
+--INSERT INTO composer(artistic_name) VALUES('Sergio Jackson');
+--
+--INSERT INTO song(release_date, tittle, url, videoclip, album_id)
+--VALUES('2017-11-17', 'Generación Perdida', 'https://open.spotify.com/track/5DsKaKaLRFOz75sQ2GMBKG?si=744e6e33d1234773', 'https://www.youtube.com/watch?v=kc3sdZlkf-o', 1);
+--INSERT INTO song_composer(song_id, composer_id)
+--VALUES(7,1), (7,2), (7,4);
+--
+--INSERT INTO song(release_date, tittle, url, videoclip, album_id)
+--VALUES('2017-11-17', 'Small Seeds', 'https://open.spotify.com/track/5RI2qZ4blYntOl87Qz7lVy?si=82e5e4c1fa9a4b46', null, 1);
+--INSERT INTO song_composer(song_id, composer_id)
+--VALUES(8,1);
+--
+--INSERT INTO song(release_date, tittle, url, videoclip, album_id)
+--VALUES('2017-11-17', 'Sentir', 'https://open.spotify.com/track/66xpZoRJntYBOFHNLuR5CY?si=8d29709599154af1', null, 1);
+--INSERT INTO song_composer(song_id, composer_id)
+--VALUES(9,1);
+--
+--INSERT INTO composer(artistic_name) VALUES('Skiz Sie7e');
+--
+--INSERT INTO song(release_date, tittle, url, videoclip, album_id)
+--VALUES('2017-11-17', 'No Me Da Vergüenza', 'https://open.spotify.com/track/13FX5JJS6YMLcog5VSY5tc?si=6158c30ca5954b95', null, 1);
+--INSERT INTO song_composer(song_id, composer_id)
+--VALUES(10,1), (10,5);
+--
+--INSERT INTO composer(artistic_name) VALUES('Sisi Greenders');
+--
+--INSERT INTO song(release_date, tittle, url, videoclip, album_id)
+--VALUES('2017-11-17', 'Naieh', 'https://open.spotify.com/track/5BKfjlUT2rXxT5rpynTuuk?si=2854d16f9e174d46', null, 1);
+--INSERT INTO song_composer(song_id, composer_id)
+--VALUES(11,1), (11,6);
+--
+--INSERT INTO song(release_date, tittle, url, videoclip, album_id)
+--VALUES('2017-11-17', 'Funk Creator', 'https://open.spotify.com/track/4kyzw6GFML34xus4by9jb7?si=14a4040517c44f3e', null, 1);
+--INSERT INTO song_composer(song_id, composer_id)
+--VALUES(12,1);
+--
+--INSERT INTO song(release_date, tittle, url, videoclip, album_id)
+--VALUES('2017-11-17', 'Dont Know Blues', 'https://open.spotify.com/track/0KqrqwHdPu08ENhyOXXlj3?si=3b636dbf9a224ad6', null, 1);
+--INSERT INTO song_composer(song_id, composer_id)
+--VALUES(13,1);
+--
+--INSERT INTO song(release_date, tittle, url, videoclip, album_id)
+--VALUES('2017-11-17', 'Seeds Riddim', 'https://open.spotify.com/track/13FX5JJS6YMLcog5VSY5tc?si=6158c30ca5954b95', null, 1);
+--INSERT INTO song_composer(song_id, composer_id)
+--VALUES(14,1);
+--
+--INSERT INTO song(release_date, tittle, url, videoclip, album_id)
+--VALUES('2017-11-17', 'Out Of Shame', 'https://open.spotify.com/track/13FX5JJS6YMLcog5VSY5tc?si=6158c30ca5954b95', null, 1);
+--INSERT INTO song_composer(song_id, composer_id)
+--VALUES(15,1);
+--
+--INSERT INTO album(name, composer_id) VALUES('Individuo', 2);
+--
+--INSERT INTO song(release_date, tittle, url, videoclip, album_id)
+--VALUES('2020-05-28', 'Proemio', 'https://aitortxumusica.bandcamp.com/track/proemio', null, 2);
+--INSERT INTO song_composer(song_id, composer_id)
+--VALUES(16,2);
+--
+--INSERT INTO song(release_date, tittle, url, videoclip, album_id)
+--VALUES('2020-05-28', 'El Individuo', 'https://aitortxumusica.bandcamp.com/track/el-individuo', 'https://www.youtube.com/watch?v=vpIPpCGc9_s', 2);
+--INSERT INTO song_composer(song_id, composer_id)
+--VALUES(17,2);
+--
+--INSERT INTO song(release_date, tittle, url, videoclip, album_id)
+--VALUES('2020-05-28', 'El Eco', 'https://aitortxumusica.bandcamp.com/track/el-eco', null, 2);
+--INSERT INTO song_composer(song_id, composer_id)
+--VALUES(18,2);
+--
+--INSERT INTO song(release_date, tittle, url, videoclip, album_id)
+--VALUES('2020-05-28', 'La Huida', 'https://aitortxumusica.bandcamp.com/track/la-huida-con-jimbo-jonez', 'https://www.youtube.com/watch?v=4P2sCBtR3Qk', 2);
+--INSERT INTO song_composer(song_id, composer_id)
+--VALUES(19,2),(19,1);
+--
+--INSERT INTO composer(artistic_name) VALUES('Soul Rebel');
+--
+--INSERT INTO song(release_date, tittle, url, videoclip, album_id)
+--VALUES('2020-05-28', 'Destierro', 'https://aitortxumusica.bandcamp.com/track/destierro-con-soul-rebel', null, 2);
+--INSERT INTO song_composer(song_id, composer_id)
+--VALUES(20,2), (20,7);
+--
+--INSERT INTO song(release_date, tittle, url, videoclip, album_id)
+--VALUES('2020-05-28', 'Renuncia', 'https://aitortxumusica.bandcamp.com/track/renuncia', null, 2);
+--INSERT INTO song_composer(song_id, composer_id)
+--VALUES(21,2);
+--
+--INSERT INTO song(release_date, tittle, url, videoclip, album_id)
+--VALUES('2020-05-28', 'Redención', 'https://aitortxumusica.bandcamp.com/track/redenci-n', null, 2);
+--INSERT INTO song_composer(song_id, composer_id)
+--VALUES(22,2);
